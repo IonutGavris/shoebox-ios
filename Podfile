938582de0c04138accb2cdfa2f57a0428b15f221
Ionut Gavris <ionut.gavris@3pillarglobal.com>
2015-11-21 14:06:35 +0200

    Added Firebase support Added Cocoa Pods support (make sure to run 'pod install' as pod files where not commited to git)

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+pod 'Firebase', '>= 2.3.2'
+
+target 'ShoeBox' do
+
+end
+
+target 'ShoeBoxTests' do
+
+end
+
+target 'ShoeBoxUITests' do
+
+end
+
--- ShoeBox/ViewController.swift
+++ ShoeBox/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Get a reference to firebase locations endpoint
+    var ref = Firebase(url: "https://shoebox.firebaseio.com/locations")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Attach a closure to read the data from firebase
+        ref.observeEventType(.Value, withBlock: { snapshot in
+                print(snapshot.value)
+            }, withCancelBlock: { error in
+                print(error.description)
+        })
     }
 
     override func didReceiveMemoryWarning() {
